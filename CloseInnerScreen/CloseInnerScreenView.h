fcecc708550e19bc32b9206553daf614d5830884
ruandao <ljy080829@gmail.com>
2019-03-08 22:14:17 +0800

    Initial Commit

---
+++ CloseInnerScreen/CloseInnerScreenView.h
@@ -0,0 +1,13 @@
+//
+//  CloseInnerScreenView.h
+//  CloseInnerScreen
+//
+//  Created by weixin on 3/8/19.
+//  Copyright © 2019 weixin. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CloseInnerScreenView : ScreenSaverView
+
+@end
